80ab980ce6fc59751ac348875af76eef2498afbe
mannshah24 <mannshah2405@gmail.com>
2025-08-14 05:13:48 +0000

    Speedrun

--- packages/nextjs/app/debug/_components/UniswapInterface.tsx
+++ packages/nextjs/app/debug/_components/UniswapInterface.tsx
@@ -189,7 +189,7 @@ export default function UniswapInterface() {
       const privateKey = process.env.NEXT_PUBLIC_PRIVATE_KEY || '';
       const signer = new ethers.Wallet(privateKey, provider);
       const signerAddress = await signer.getAddress();
-      const uniswapContractAddress = "0x40b694c75b4e509aa8cbe9993a37af9546ce36e8";
+      const uniswapContractAddress="0xd4096688cffc51a5448d7c5c94718ee70c6a2ee1";
       const uniswapContract = new ethers.Contract(uniswapContractAddress, IUniswapV2, signer);
       console.log("contract", uniswapContract);
       setContract(uniswapContract);
